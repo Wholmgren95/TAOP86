09f9d5e223d1aa863e9e4d7b80a2b8e0d433e9d2
Wille <wholmgren95@hotmail.com>
2018-09-24 19:25:34 +0200

    asd

--- venv/Labb_2.py
+++ venv/Labb_2.py
@@ -4,7 +4,7 @@ import sys
 
 filc = " ".join(sys.argv[1:]).split('.')[0] + '.npz'
 npzfile = np.load(filc)
-c = npzfile['c']*-1
+c = npzfile['c']
 b = npzfile['b']
 A = npzfile['A']
 bix = npzfile['bix'] #KOlumnindex för tillåten startbas
@@ -29,24 +29,25 @@ cN = c[nix]
 iter = 0
 while iter >= 0:
     iter += 1
-    in_col=np.argmax(c)
+    in_col=np.argmin(c)
+    rc_min = c[in_col]
     ut_list=[]
 
     for a_rows,b_rows in zip(A,b):
         ut_list.append(b_rows/a_rows[in_col])
 
-    ut_row = ut_list.index(min(ut_list))
-    bix[ut_row] = in_col
-    A[ut_row] = A[ut_row]/A[ut_row][in_col] #hela raden delat på minsta
-
-    for i in range(len(A[0])):
-        temp = A[i][in_col]
-        if i is not ut_row:
-            A[i] = A[i] - temp*A[ut_row]
-
-    for a_rows,b_rows in zip(A,b):
-        temp = a_rows[in_col]
-        a_rows = a_rows - temp*A[ut_row]
+    inkvar = ut_list.index(min(ut_list))
+    # bix[inkvar] = in_col
+    # A[inkvar] = A[inkvar]/A[inkvar][in_col] #hela raden delat på minsta
+    #
+    # for i in range(len(A[0])):
+    #     temp = A[i][in_col]
+    #     if i is not inkvar:
+    #         A[i] = A[i] - temp*A[inkvar]
+    #
+    # for a_rows,b_rows in zip(A,b):
+    #     temp = a_rows[in_col]
+    #     a_rows = a_rows - temp*A[inkvar]
 
 
 
@@ -57,34 +58,34 @@ while iter >= 0:
     # and index for entering variable, inkvar
     # --------
 
-    # if rc_min >= -1.0E-12:
-    #     print('Ready')
-    #     iter = -1
-    #
-    #     # construct solution, x, and check it
-    #     # --------
-    #
-    #     diffx = np.linalg.norm(x - xcheat)
-    #     diffz = z - zcheat
-    #     print('xdiff: ' + repr(diffx))
-    #     print('zdiff: ' + repr(diffz))
-    # else:
-    #     # calc entering column, a
-    #     # --------
-    #
-    #     if max(a) <= 0:
-    #         # unbounded solution
-    #         print('Unbounded solution!')
-    #         iter = -1
-    #     else:
-    #         # calc leaving var, utgvar
-    #         # --------
-    #
-    #         print(' Iter: ' + repr(iter) + ' z: ' + repr(z) + ' rc: ' + repr(rc_min) + ' ink: ' + repr(
-    #             inkvar + 1) + ' utg: ' + repr(utgvar + 1))
-    #
-    #         # make new partition
-    #         # --------
+    if rc_min >= -1.0E-12:
+        print('Ready')
+        iter = -1
 
-#elapsed = time.time() - t1
-#print('Elapsed time: ' + repr(elapsed))
+        # construct solution, x, and check it
+        # --------
+
+        diffx = np.linalg.norm(x - xcheat)
+        diffz = z - zcheat
+        print('xdiff: ' + repr(diffx))
+        print('zdiff: ' + repr(diffz))
+    else:
+        # calc entering column, a
+        # --------
+
+        if max(a) <= 0:
+            # unbounded solution
+            print('Unbounded solution!')
+            iter = -1
+        else:
+            # calc leaving var, utgvar
+            # --------
+
+            print(' Iter: ' + repr(iter) + ' z: ' + repr(z) + ' rc: ' + repr(rc_min) + ' ink: ' + repr(
+                inkvar + 1) + ' utg: ' + repr(utgvar + 1))
+
+            # make new partition
+            # --------
+
+elapsed = time.time() - t1
+print('Elapsed time: ' + repr(elapsed))
