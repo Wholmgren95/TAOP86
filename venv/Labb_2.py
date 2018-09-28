4131ccb2daae1d76d44a9e13e663f0713ed1f5af
wholmgren95 <wholmgren95@hotmail.com>
2018-09-28 15:53:22 +0200

    fml

--- venv/Labb_2.py
+++ venv/Labb_2.py
@@ -4,7 +4,7 @@ import sys
 
 filc = " ".join(sys.argv[1:]).split('.')[0] + '.npz'
 npzfile = np.load(filc)
-c = npzfile['c']
+c = npzfile['c']*-1
 b = npzfile['b']
 A = npzfile['A']
 bix = npzfile['bix'] #KOlumnindex för tillåten startbas
@@ -23,9 +23,9 @@ B = A[:, bix]
 N = A[:, nix]
 cB = c[bix]
 cN = c[nix]
-xB = np.transpose(bix)
-xN = np.transpose(nix)
-x = 2 #xn + xb
+
+diffx=0
+diffz=0
 # Start iterations
 iter = 0
 while iter >= 0:
@@ -48,7 +48,8 @@ while iter >= 0:
 
     B_inv = np.linalg.inv(B)
     xB = np.dot(B_inv, b)
-    xN = 0
+
+
     y = np.transpose(np.dot(np.transpose(cB),B_inv))
     z = np.dot(np.transpose(b),y)
     c_hattN = cN - (np.dot(np.transpose(N),y)) #np.transpose(N)
@@ -57,18 +58,18 @@ while iter >= 0:
     # and index for entering variable, inkvar
     # --------
 
-    inkvar = np.argmin(c)
-    rc_min = c[inkvar]
+    inkvar = np.argmax(c)
+    rc_min = c[inkvar]*-1
 
     
-    for e in c_hattN:
-        if e <=0:
-            break
+
+    if(c_hattN <=0).all():
+        iter=-1
     if rc_min >= -1.0E-12:
         print('Ready')
         iter = -1
 
-        bix[utgvar] = inkvar
+
             # construct solution, x, and check it
             # --------
         diffx = np.linalg.norm(x - xcheat)
@@ -80,7 +81,7 @@ while iter >= 0:
         # --------
         # a = inkommande kolumn hatt
         # A[:,inkvar] är att ta ut kolumn ur A på plats inkvar
-        a = np.dot(B_inv, A[:, inkvar])
+        a = np.dot(B_inv, N[:, inkvar]) #A ist för N
 
         if max(a) <= 0:
             # unbounded solution
@@ -91,7 +92,7 @@ while iter >= 0:
             # --------
             ut_list = []
 
-            for a_rows, b_rows in zip(A, b):
+            for a_rows, b_rows in zip(N, b):
                 ut_list.append(b_rows / a_rows[inkvar])
 
             utgvar = ut_list.index(min(ut_list))
@@ -102,6 +103,18 @@ while iter >= 0:
             # make new partition
             # --------
 
+            #bix[utgvar] = inkvar
+            bix[utgvar], nix[inkvar] = nix[inkvar],bix[utgvar]
+            temp = np.copy(B[:, utgvar])
+            B[:, utgvar] = N[:, inkvar]
+            N[:, inkvar] = temp
+            N = np.roll(N,1,1)
+            cN[inkvar], cB[utgvar] = cB[utgvar], cN[inkvar]
+            cN = np.roll(cN,1,0)
+
+
 
 elapsed = time.time() - t1
 print('Elapsed time: ' + repr(elapsed))
+print(zcheat)
+print("z diff:",z - zcheat)
